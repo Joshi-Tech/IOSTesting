13a1201dbb9e9ac86146d9dcd218bbc48c02126b
laxmi kant joshi <lkjoshi@Mac.broadband>
2025-09-04 20:05:29 +0100

    updates

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -2,28 +2,38 @@ package tests;
 
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.options.XCUITestOptions;
-import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class BaseTest {
+    protected static IOSDriver driver;
 
-    public static WebDriver driver;
+    @BeforeMethod
+    public IOSDriver setUp() throws MalformedURLException {
+        // Read from system properties, fallback to defaults
+        String deviceName = System.getProperty("deviceName", "iPhone 16 Pro");
+        String platformVersion = System.getProperty("platformVersion", "18.6");
+        String appPath = System.getProperty("appPath",
+                "/Users/lkjoshi/Library/Developer/Xcode/DerivedData/UICatalog-gwynisoydgnpybcuqfqtlmtnfhrg/Build/Products/Debug-iphonesimulator/UICatalog.app");
+        String serverPath = System.getProperty("server.path", "http://127.0.0.1:4723/wd/hub");
 
-    public WebDriver initializeDriver() throws MalformedURLException {
-        XCUITestOptions options = new XCUITestOptions();
-        options.setDeviceName("iPhone 15 Pro");
-        options.setPlatformVersion("17.5");
+        XCUITestOptions options = new XCUITestOptions()
+                .setPlatformName("iOS")
+                .setPlatformVersion(platformVersion)
+                .setDeviceName(deviceName)
+                .setAutomationName("XCUITest")
+                .setApp(appPath);
 
-        // Allow overriding the app path via -Dapp.path
-        options.setApp(System.getProperty("app.path", "/Users/qa/Documents/UICatalog.app"));
-        options.setPlatformName("iOS");
+        return driver = new IOSDriver(new URL(serverPath), options);
+    }
 
-        // Allow overriding the base path via -Dserver.path
-        String basePath = System.getProperty("server.path", "");
-        String serverUrl = "http://127.0.0.1:4723" + basePath;
-
-        return driver = new IOSDriver(new URL(serverUrl), options);
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
