c582a16930b5bdb1c49a92fe3d1e7d26d1c980e4
laxmi kant joshi <lkjoshi@Mac.broadband>
2025-09-04 07:23:12 +0100

    updated BaseTest

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -13,6 +13,54 @@ public class BaseTest {
 
     @BeforeMethod
     public IOSDriver setUp() throws MalformedURLException {
+        // ✅ Read app path from system property (set in GitHub Actions)
+        // If not set, fall back to your local DerivedData path
+        String appPath = System.getProperty(
+                "app.path",
+                "/Users/lkjoshi/Library/Developer/Xcode/DerivedData/UICatalog-gwynisoydgnpybcuqfqtlmtnfhrg/Build/Products/Debug-iphonesimulator/UICatalog.app"
+        );
+
+        System.out.println("🚀 Using app from: " + appPath);
+
+        XCUITestOptions options = new XCUITestOptions()
+                .setPlatformName("iOS")
+                .setPlatformVersion("18.6")
+                .setDeviceName("iPhone 16 Pro")
+                .setAutomationName("XCUITest")
+                .setApp(appPath);
+
+        return driver = new IOSDriver(new URL("http://127.0.0.1:4723"), options);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
+
+
+
+
+
+
+/*
+package tests;
+
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.options.XCUITestOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class BaseTest {
+    protected static IOSDriver driver;
+
+    @BeforeMethod
+    public IOSDriver setUp() throws MalformedURLException {
         XCUITestOptions options = new XCUITestOptions()
                 .setPlatformName("iOS")
                 .setPlatformVersion("18.6")
@@ -30,3 +78,4 @@ public class BaseTest {
         }
     }
 }
+*/
